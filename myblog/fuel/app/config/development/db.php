f65b0768029e1451f392459bfbeadfcc42241b01
Takaesu Junya <takaesu_junya@ca-adv.co.jp>
2021-06-10 00:03:15 +0900

    Update db connection information

--- myblog/fuel/app/config/development/db.php
+++ myblog/fuel/app/config/development/db.php
@@ -22,9 +22,9 @@
 return array(
 	'default' => array(
 		'connection' => array(
-			'dsn'      => 'mysql:host=localhost;dbname=fuel_dev',
+			'dsn'      => 'mysql:host=mysql;dbname=myblog',
 			'username' => 'root',
-			'password' => 'root',
+			'password' => 'password',
 		),
 	),
 );
